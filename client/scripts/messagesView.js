00f33da2e5a06a2d3495b914da2ab324f94e4ae3
pfaithmtan <pfaithmtan@gmail.com>
2019-09-13 20:02:13 -0700

    Pass ajax and chatroom behavior tests

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,26 @@ var MessagesView = {
   },
 
   render: function() {
+  },
+
+  renderMessage: function(message) {
+    var $message = $('<div class="message"></div>');
+    $message.append(`<div class="username"> ${message.username} </div>`);
+    $message.append(`<div class="text"> ${message.text} </div>`);
+    $message.append(`<div class="roomname"> ${message.roomname} </div>`);
+    $( "#chats" ).append($message);
   }
 
+
 };
+
+// it('should be able to add messages to the DOM', function() {
+//   var message = {
+//     username: 'Mel Brooks',
+//     text: 'Never underestimate the power of the Schwartz!',
+//     roomname: 'lobby'
+//   };
+//   MessagesView.renderMessage(message);
+//   expect($('#chats').children().length).to.equal(1);
+// });
+
